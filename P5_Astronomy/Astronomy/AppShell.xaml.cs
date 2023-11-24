524bcb7a78953dea1f70365b6d9f78a786f94386
Atari96 <pmorato10@yahoo.es>
2023-11-25 00:59:45 +0100

    V1

---
+++ P5_Astronomy/Astronomy/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Astronomy.Pages;
+
+namespace Astronomy;
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute("astronomicalbodydetails", typeof(AstronomicalBodyPage));
+
+
+        }
+        
+    }
+    
+
