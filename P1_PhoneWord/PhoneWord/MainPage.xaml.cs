7c1ebb9c4395e7ebfbb2799c490044b08aa587c3
Atari96 <pmorato10@yahoo.es>
2023-11-25 01:04:20 +0100

    V1

---
+++ P1_PhoneWord/PhoneWord/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿namespace PhoneWord
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        string translatedNumber;
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = PhoneNumberText.Text;
+            translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                CallButton.IsEnabled = true;
+                CallButton.Text = "Llamar " + translatedNumber;
+            }
+            else
+            {
+                CallButton.IsEnabled = false;
+                CallButton.Text = "Llamar";
+            }
+        }
+        async void OnCall(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert(
+        "Marcar número",
+        "Quieres llamar " + translatedNumber + "?",
+        "Si",
+        "No"))
+            {
+                try
+                {
+                    if (PhoneDialer.Default.IsSupported)
+                        PhoneDialer.Default.Open(translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("No se puede marcar", "Número inválido.", "OK");
+                }
+                catch (Exception)
+                {
+                    // Other error has occurred.
+                    await DisplayAlert("No se puede marcar", "Fallo marcado de número.", "OK");
+                }
+            }
+        }
+
+    }
+
+}
