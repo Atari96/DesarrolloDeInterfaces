9baf41515c0624416df60f17be46180fc1fe20ad
Atari96 <pmorato10@yahoo.es>
2023-12-02 03:29:28 +0100

    V1.1

---
+++ BlazorHybridPmp/BlazorHybridPmp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorHybridPmp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ BlazorHybridPmp/BlazorHybridPmp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BlazorHybridPmp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
