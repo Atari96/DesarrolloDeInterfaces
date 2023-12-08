67c8ab7c040aeb8e817c2cacb55e9055ab84d7e4
Atari96 <pmorato10@yahoo.es>
2023-12-08 01:49:15 +0100

    v2

---
+++ MiLogIn/LoginDeMiWeb/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginDeMiWeb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private object registroWindow;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            // Limpiar el contenido del cuadro de texto
+            textUsuario.Text = string.Empty;
+
+            // Limpiar el contenido del password
+            textPassword.Password = string.Empty;
+
+            // Desmarcar CheckBox
+            CheckBox1.IsChecked = false; 
+
+        }
+
+        private void btnEnviar_Click(object sender, RoutedEventArgs e)
+        {
+            String usuario = textUsuario.Text;
+            String password = textPassword.Password;
+        }
+
+        private void registro(object sender, MouseButtonEventArgs e)
+        {
+            Registro registroWindow = new Registro();
+            registroWindow.Show();
+        }
+
+        private void recuperar(object sender, MouseButtonEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+    }
+}
