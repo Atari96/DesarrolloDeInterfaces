3f46fb91919095b4957a1664b2aa6b055fcbec40
Atari96 <pmorato10@yahoo.es>
2023-12-11 23:11:45 +0100

    Version entregada

--- MiLogIn/LoginDeMiWeb/MainWindow.xaml.cs
+++ MiLogIn/LoginDeMiWeb/MainWindow.xaml.cs
@@ -12,19 +12,32 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace LoginDeMiWeb
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    public class Usuario
+    {
+        public string usuario { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+    }
     public partial class MainWindow : Window
     {
-        private object registroWindow;
+        private object registroWindow = new object();
+        private List<Usuario> listaUsuarios = new List<Usuario>();
 
         public MainWindow()
         {
             InitializeComponent();
+            CargarUsuarios();
+        }
+
+        private void CargarUsuarios()
+        {
+            string rutaArchivo = @"DatosLogin\listadoPersonas.json"; 
+            string contenidoJson = File.ReadAllText(rutaArchivo);
+            listaUsuarios = JsonConvert.DeserializeObject<List<Usuario>>(contenidoJson);
         }
 
         private void btnReset_Click(object sender, RoutedEventArgs e)
@@ -44,18 +57,33 @@ namespace LoginDeMiWeb
         {
             String usuario = textUsuario.Text;
             String password = textPassword.Password;
+
+            if (ValidarCredenciales(usuario, password))
+            {
+                MessageBox.Show("¡Olé, Bienvenido a la web!");
+            }
+            else
+            {
+                MessageBox.Show("Error, usuario o contraseña incorrectos");
+            }
+        }
+
+        private bool ValidarCredenciales(string usuario, string password)
+        {
+            // Verificar coincidencia usuarios y contraseñas
+            return listaUsuarios.Any(u => u.usuario == usuario && u.password == password);
         }
 
         private void registro(object sender, MouseButtonEventArgs e)
         {
             Registro registroWindow = new Registro();
-            registroWindow.Show();
+            registroWindow.ShowDialog();
         }
 
         private void recuperar(object sender, MouseButtonEventArgs e)
         {
             Window1 window1 = new Window1();
-            window1.Show();
+            window1.ShowDialog();
         }
     }
 }
