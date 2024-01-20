c7fe077b71a1402796e48ac95d8b8966217c78f2
Atari96 <pmorato10@yahoo.es>
2024-01-20 19:22:46 +0100

    V1 paso 7

--- P9_PizzaShop/P9_01_Inicio/BlazingPizza.Client/Program.cs
+++ P9_PizzaShop/P9_01_Inicio/BlazingPizza.Client/Program.cs
@@ -2,18 +2,20 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<OrderState>();
+
 
 // Añadimos un servicio que nos permita configurar e inyectar el httpclient y la direccion, con el gestor que nos permita
 // agregar los encabezados de autorizacion
 builder.Services.AddHttpClient<OrdersClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+builder.Services.AddScoped<OrderState>();
 
 // Llamada para habilitar los servicios de autenticacion, incluimoss el <PizzaAuthenticationState>
 // La parte options implica el codigo necesario para poder hacer el logout
